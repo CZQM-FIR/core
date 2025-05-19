117f0903e0883b53d6433f4f304eb673b968fbab
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-05-19 16:58:34 -0400

    attempt to fix build issue

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
     port: 5100
+  },
+  resolve: {
+    alias: {
+      '@czqm/db/schema': path.resolve(__dirname, '../../packages/db/dist/index.js')
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['@czqm/db/schema']
+    }
   }
 });
