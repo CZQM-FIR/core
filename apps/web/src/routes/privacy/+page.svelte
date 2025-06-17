3267ff0ad2a480b0531a0295e0dfe2efec0e2126
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-06-17 16:24:35 -0400

    feat: privacy policy

---
+++ apps/web/src/routes/privacy/+page.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+</script>
+
+<section>
+  <div class="prose container mx-auto max-w-240 py-6">
+    <h1>CZQM / QX Privacy Policy</h1>
+    <p>
+      When you use CZQM / QX Moncton / Gander services such as but not limited to czqm.ca, and the
+      CZQM Discord, there is information sent to us and stored to allow us to provide services to
+      you. Please familiarize yourself with our full privacy policy below.
+    </p>
+    <h2>Collection</h2>
+    <p>
+      Information is collected from a variety of sources, many of which do not come directly from
+      the CZQM website.
+    </p>
+    <h3>VATSIM</h3>
+    <p>
+      When you register an account with VATSIM, a number of pieces of information, including but not
+      limited to full name and email address, are collected and associated with your account. When
+      you log into the CZQM website with your VATSIM, this information is then accessed and stored
+      by CZQM. This information is accessed and updated every time you log in to the CZQM website.
+    </p>
+    <p>
+      Information regarding the connection sessions of our members is also stored. This information
+      is used to monitor compliance with our other policies and to analyze statistics regarding our
+      members activity on the VATSIM network. Whenever a member of the CZQM FIR connects to the
+      VATSIM network as a controller, the following information is stored:
+    </p>
+    <ul>
+      <li>Connection Start Time</li>
+      <li>Connection Duration</li>
+      <li>Callsign</li>
+      <li>Frequency</li>
+    </ul>
+    <h3>VATCAN</h3>
+    <p>
+      When you transfer to the CZQM FIR through <a href="https://vatcan.ca">VATCAN.ca</a>, your
+      name, email address, and other VATSIM account information are collected and stored by CZQM.
+      This information is accessed and updated daily.
+    </p>
+    <h2>Usage</h2>
+    <p>
+      To allow the function of the core CZQM web services, we utilize your information across
+      several of our services. We will never share your data unless required by law. The information
+      we collect is maintained with confidentiality to the extent possible.
+    </p>
+    <p>The following account information may be shared publicly:</p>
+    <ul>
+      <li>VATSIM Certificate ID (CID)</li>
+      <li>Your full name</li>
+      <li>Your vatsim rating and account status</li>
+      <li>VATCAN Facility Association</li>
+      <li>Your VATSIM controlling sessions</li>
+    </ul>
+    <h2>Cookies</h2>
+    <p>
+      A <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Cookies">browser cookie</a
+      > entitled "session" will be stored in your browser when signed in to czqm.ca and other CZQM subdomains.
+      This cookie is required for basic web functionality and may not be opted out of. This cookie will
+      never be used to track users across non-CZQM operated websites. 3rd Party Cookies will not be used.
+    </p>
+    <p>
+      You may choose to disable cookie usage via your browser, but doing so will prevent access and
+      use across restricted areas of the website and severely degrade your experience and place you
+      at risk of violating other CZQM policies by not being able to fulfil member requirements.
+    </p>
+    <h2>Your Rights Regarding Your Data</h2>
+    <p>
+      As a sub-division of VATCAN and VATSIM, you are entitled to the same rights as provided to you
+      by the VATSIM Network and as required by PIPEDA and GDPR (EU Citizens Only).
+    </p>
+    <h3>Request for Access</h3>
+    <p>
+      You have a right to access your data and view the data collected on you. This data will be
+      released to you on written request to <a href="mailto:webmaster@czqm.ca">webmaster@czqm.ca</a
+      >.
+    </p>
+    <p>
+      Data requests will be honoured and delivered within one month. In the event CZQM cannot meet
+      the one month timeframe, CZQM will email you prior to the due date for a further two month
+      extension from the time of your initial request.
+    </p>
+    <p>
+      Only personal information directly relating to the member will be delivered. To protect the
+      privacy of CZQM staff members and other members, the names of individual members who may
+      appear in your data may be redacted.
+    </p>
+    <p>Request for access to your information will be provided to you at no charge.</p>
+    <h3>Right to Rectify</h3>
+
+    You have a right to rectify data you believe to be inaccurate. We encourage members to ensure
+    details such as email address are kept up to date through their VATSIM account. If you believe
+    other data held is incorrect you may contact the CZQM staff team to seek to have it corrected.
+    To request data rectification, email CZQM at<a href="mailto:webmaster@czqm.ca"
+      >webmaster@czqm.ca</a
+    >.
+    <p>All changes will be completed at no charge to you.</p>
+    <p>
+      You have the right to request we restrict the processing of your data in the event of a
+      dispute, this may restrict your ability to connect to the network and your status as an active
+      CZQM controller (if applicable).
+    </p>
+    <p>
+      You have the right to object to our legitimate interests. While your objection is being
+      considered, processing of your data may be frozen, and this may restrict your ability to
+      connect to the network and your status as an active CZQM controller (if applicable).
+    </p>
+    <h3>Right to be Forgotten</h3>
+    <p>
+      You have the right to request the erasure of your data from our systems. This will result in
+      the termination of your controller or visitor status with CZQM (if applicable). Should you
+      continue to access the CZQM website, your data will be re-collected from VATSIM and VATCAN.
+    </p>
+    <p>
+      Should you wish for your data to be removed from VATCAN or VATSIM, please contact them
+      directly as outlined in their privacy policies.
+    </p>
+    <h2>Changes to this Document</h2>
+    <p>Changes to this privacy policy will be communicated to all members.</p>
+    <p>
+      Any questions regarding this document shall be directed to the CZQM staff team via <a
+        href="mailto:staff@czqm.ca">staff@czqm.ca</a
+      >
+    </p>
+  </div>
+</section>
