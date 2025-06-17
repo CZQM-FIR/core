6ee18c88ad6293d66dcccfa31377c020d945104b
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-06-17 16:31:06 -0400

    feat: branding policy

---
+++ apps/web/src/routes/(policies)/branding/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts"></script>
+
+<section>
+  <div class="prose container mx-auto max-w-240 py-6">
+    <h1>Branding</h1>
+    <p>
+      Any requests to use the CZQM logo, the Moncton / Gander FIR or CZQM / QX FIR name, any of our
+      published images, tools or services, or any other use of our brand identity shall be directed
+      to <a href="mailto:staff@czqm.ca">staff@czqm.ca</a>. Any use without consent shall be
+      considered a violation of our copyright.
+    </p>
+  </div>
+</section>
