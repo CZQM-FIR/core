6c1f276570de307871f9533ee0bed80b0976b0a3
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-11-12 22:57:27 -0500

    add info text

--- apps/vector/src/routes/waitlist/+page.svelte
+++ apps/vector/src/routes/waitlist/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class="container mx-auto my-6">
-	<h1 class="text-2xl font-semibold">Hey there, {(await getCurrentUserInfo()).name_first}!</h1>
+	<h1 class="text-3xl font-semibold">Hey there, {(await getCurrentUserInfo()).name_first}!</h1>
 
 	{#await getIndividualsWaitlistEntries()}
 		<p>Loding your wait list information...</p>
@@ -16,16 +16,27 @@
 			</p>
 		{:else}
 			{#each waitlistEntries as entry (entry.id)}
-				<p class="my-4">
-					You are currently on the <strong class="text-blue-400">{entry.waitlist.name}</strong>
-					waitlist. You are currently number
-					<strong class="text-blue-400">{entry.position + 1}</strong>
-					in line.
-					{#if entry.waitlist.waitTime}
-						The estimated wait time for someone joining the waitlist now is approximately
-						<strong class="text-blue-400">{entry.waitlist.waitTime}</strong>.
-					{/if}
-				</p>
+				<div class="my-4 flex flex-col gap-5 text-lg">
+					<p>
+						You are currently on the <strong class="text-blue-400">{entry.waitlist.name}</strong>
+						waitlist. You are currently number
+						<strong class="text-blue-400">{entry.position + 1}</strong>
+						in line.
+						{#if entry.waitlist.waitTime}
+							The estimated wait time for someone joining the waitlist now is approximately
+							<strong class="text-blue-400">{entry.waitlist.waitTime}</strong>.
+						{/if}
+					</p>
+					<p>
+						Please note that the waitlist times, if listed, are estimates. Waitlists are subject to
+						Instructor availability. We are a 100% volunteer team, so please be patient. Please do
+						not reach out to Instructors or Mentors regarding training unless they reach out first.
+					</p>
+					<p>
+						Our waitlist operates in a first come first serve basis. You will be contacted when you
+						may begin training along with further instructions.
+					</p>
+				</div>
 			{/each}
 		{/if}
 	{:catch error}
