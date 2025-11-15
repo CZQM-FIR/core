f34505c153003a0eade9a67d85bd7c82360daa48
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-11-15 09:49:22 -0500

    create waitlists

--- apps/vector/src/routes/a/waitlist/+page.svelte
+++ apps/vector/src/routes/a/waitlist/+page.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
+	import { getWaitlists } from '$lib/remote/waitlist.remote';
+	import { ListPlus } from '@lucide/svelte';
 </script>
 
 <section class="container mx-auto py-5">
-	<h1 class="text-2xl font-semibold">Wait List Management</h1>
+	<div class="flex flex-row items-baseline justify-between">
+		<h1 class="text-2xl font-semibold">Wait List Management</h1>
+		<a class="btn btn-success" href="/a/waitlist/create"><ListPlus /></a>
+	</div>
 	<div class="divider my-0"></div>
 	<p class="py-3">Please select a waitlist:</p>
-	<ul class="flex flex-row gap-3">
-		{#each data.waitlists as waitlist (waitlist.id)}
-			<li class="text-lg">
-				<a href="/a/waitlist/{waitlist.id}" class="btn btn-primary">
-					<span class="font-bold">{waitlist.name}</span> - {waitlist.students.length} student{waitlist
-						.students.length === 1
-						? ''
-						: 's'}
-				</a>
-			</li>
-		{/each}
+	<ul class="flex flex-row flex-wrap gap-3">
+		{#await getWaitlists()}
+			<p>Loading Waitlists...</p>
+		{:then waitlists}
+			{#each waitlists as waitlist (waitlist.id)}
+				<li class="text-lg">
+					<a href="/a/waitlist/{waitlist.id}" class="btn btn-primary">
+						<span class="font-bold">{waitlist.name}</span> - {waitlist.students.length} student{waitlist
+							.students.length === 1
+							? ''
+							: 's'}
+					</a>
+				</li>
+			{/each}
+		{:catch error}
+			<p>There was an error loading the waitlists: {error}</p>
+		{/await}
 	</ul>
 </section>
---
+++ apps/vector/src/routes/a/waitlist/create/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { createWaitlist } from '$lib/remote/waitlist.remote';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<section class="container mx-auto py-5">
+	<h1 class="text-2xl font-semibold">Create Wait List</h1>
+	<div class="divider mt-0"></div>
+
+	<form {...createWaitlist}>
+		<fieldset class="fieldset my-4">
+			<legend class="fieldset-legend">Wait List Name</legend>
+			<input type="text" class="input" name="name" required />
+			<p class="text-error text-xs">* Required</p>
+		</fieldset>
+
+		<fieldset class="fieldset my-4">
+			<legend>Moodle Wait List Cohort</legend>
+			<input type="text" class="input" name="wcohort" />
+			<p class=" text-xs">
+				The cohort that users will be added to when they are added to the waitlist.
+			</p>
+		</fieldset>
+
+		<fieldset class="fieldset my-4">
+			<legend>Moodle Cohort</legend>
+			<input type="text" class="input" name="cohort" />
+			<p class=" text-xs">
+				The cohort that users will be added to when they are enrolled in the course.
+			</p>
+		</fieldset>
+
+		<button class="btn btn-primary w-min">Create</button>
+	</form>
+</section>
