0ed3eea957b5fa46e404c6a53f49cd28ca9c1c0c
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-12-12 22:21:38 -0800

    fix: remove merge artifacts

--- packages/db/src/schema/notifications.ts
+++ packages/db/src/schema/notifications.ts
@@ -1,4 +1,3 @@
-<<<<<<< ours
 import { relations, type InferSelectModel } from "drizzle-orm";
 import { index, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { users } from "./index";
@@ -50,47 +49,3 @@ export const notificationsRelations = relations(
 );
 
 export type Notification = InferSelectModel<typeof notifications>;
-|||||||
-=======
-import { relations, type InferSelectModel } from "drizzle-orm";
-import { index, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { users } from "./index";
-
-export const notifications = sqliteTable(
-  "notifications",
-  {
-    id: text()
-      .primaryKey()
-      .$defaultFn(() => crypto.randomUUID()),
-    timestamp: int({ mode: "timestamp" }).notNull(),
-    userId: int()
-      .notNull()
-      .references(() => users.cid),
-    type: text().notNull(),
-    message: text().notNull(),
-    sent: int({ mode: "timestamp" }),
-  },
-  (table) => {
-    return {
-      userIdIdx: index("notifications_user_id_idx").on(table.userId),
-      timestampIdx: index("notifications_timestamp_idx").on(table.timestamp),
-      userTimestampIdx: index("notifications_user_timestamp_idx").on(
-        table.userId,
-        table.timestamp
-      ),
-    };
-  }
-);
-
-export const notificationsRelations = relations(
-  notifications,
-  ({ many, one }) => ({
-    user: one(users, {
-      fields: [notifications.userId],
-      references: [users.cid],
-    }),
-  })
-);
-
-export type Notification = InferSelectModel<typeof notifications>;
->>>>>>> theirs
