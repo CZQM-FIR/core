439af1549ad39ee16a01ac517d371883d428c404
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2026-03-18 21:58:53 -0700

    add create document

--- apps/overseer/src/routes/a/dms/groups/[id]/+page.svelte
+++ apps/overseer/src/routes/a/dms/groups/[id]/+page.svelte
@@ -26,12 +26,18 @@
 		<a href="/a/dms" class="text-primary hover:link flex flex-row items-center gap-1">
 			<ChevronLeft size="15" /> Back to DMS
 		</a>
+		<div class="mt-4">
+			<a href={`/a/dms/groups/${id}/new-document`} class="btn btn-primary">Create Document</a>
+		</div>
 		<div class="divider"></div>
 
 		<div class="mt-5 overflow-x-auto">
 			{#await getDocumentsByGroup(id)}
 				<p>Loading documents...</p>
 			{:then documents}
+				{@const sortedDocuments = [...documents].sort(
+					(a, b) => (a.sort ?? 99) - (b.sort ?? 99) || a.name.localeCompare(b.name)
+				)}
 				<table class="table">
 					<thead>
 						<tr>
@@ -43,20 +49,24 @@
 						</tr>
 					</thead>
 					<tbody>
-						{#if documents.length === 0}
+						{#if sortedDocuments.length === 0}
 							<tr>
 								<td colspan="5" class="text-base-content/70 py-6 text-center">
 									No documents found for this group.
 								</td>
 							</tr>
 						{:else}
-							{#each documents as { id: documentId, name, short, required, sort }, index (documentId)}
+							{#each sortedDocuments as { id: documentId, name, short, required, sort, group }, index (documentId)}
 								<tr>
 									<td>{index + 1}</td>
 									<td>{name}</td>
-									<td>{short ?? '-'}</td>
+									{#if short}
+										<td>{group?.slug} / {short}</td>
+									{:else}
+										<td>-</td>
+									{/if}
 									<td>{required ? 'Yes' : 'No'}</td>
-									<td>{sort}</td>
+									<td>{sort ?? 99}</td>
 								</tr>
 							{/each}
 						{/if}
