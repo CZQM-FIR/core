9c9fe437b67585a7b5c2db58ff61105a594ef5bf
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2026-09-20 18:22:38 -0700

    chore: lint

--- apps/overseer/src/lib/auth.ts
+++ apps/overseer/src/lib/auth.ts
@@ -82,7 +82,8 @@ export function auth(event: RequestEvent): Promise<SessionValidationResult> {
 }
 
 export type SessionValidationResult =
-	{ session: AuthSession; user: User } | { session: null; user: null };
+	| { session: AuthSession; user: User }
+	| { session: null; user: null };
 
 export type UserWithRelations = User & {
 	flags: Array<{
--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -82,4 +82,5 @@ export function auth(event: RequestEvent): Promise<SessionValidationResult> {
 }
 
 export type SessionValidationResult =
-  { session: AuthSession; user: User } | { session: null; user: null };
+  | { session: AuthSession; user: User }
+  | { session: null; user: null };
